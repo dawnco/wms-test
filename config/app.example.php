13a464b405fd6b07fc607abf1c556de724b1ee07
Daw <abke@qq.com>
2022-07-31 18:15:38 +0800

    new

---
+++ config/app.example.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author Dawnc
+ * @date   2020-12-08
+ */
+return [
+    "env" => "dev",   // 环境 dev 开发  production 生产
+    "base_uri" => "/",
+    "shell_dir" => dirname(__DIR__) . "/app/Shell",
+    "exception" => [
+        "handler" => \App\Exception\AppExceptionHandler::class,
+    ],
+    "log" => [
+        'level' => "debug", // 日志登录  debug info  warning error
+        'dir' => dirname(__DIR__) . "/log", // 日志目录
+    ],
+    "db" => [
+        "default" => [
+            "hostname" => "8.219.113.230",
+            "username" => "dev",
+            "password" => "dev@LLC.123",
+            "database" => "loan_core",
+            "port" => 3306,
+            "charset" => "utf8mb4",
+        ],
+    ],
+    'redis' => [
+        'default' => [
+            "hostname" => "127.0.0.1",
+            "port" => 6379,
+            "password" => ""
+        ]
+    ],
+    "aliyun" => [
+
+    ]
+
+];
